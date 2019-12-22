2dba1f230219810152776620030d43be15791ddb
Alim <abaev.alim@yandex.ru>
2019-12-22 18:38:34 +0300

    add method in IDevice to set device addresses

--- Driver/Headers/IDevice.h
+++ Driver/Headers/IDevice.h
@@ -2,12 +2,16 @@
 
 #include "types.h"
 
+#include <string>
+
 namespace _onvif
 {
 	class IDevice
 	{
 	public:
 		virtual void Init() = 0;
+
+		virtual void SetAddressInfo(const std::string& ip, short port, const std::string& deviceServiceURI) = 0;
 
 		virtual void SetCreds(const char* login, const char* pass) = 0;
 
@@ -18,5 +22,12 @@ namespace _onvif
 	protected:		
 		Services services_;
 		CapabilitiesSP capabilities_;
+
+		std::string ip_;
+		short port_;
+		std::string device_service_uri_;
+
+		std::string login_;
+		std::string pass_;
 	};
 }
