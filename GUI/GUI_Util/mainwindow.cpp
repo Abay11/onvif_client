0f3f4faf2936ee59908624949cc2b2f017f646f5
Alim <abaev.alim@yandex.ru>
2019-12-22 14:15:52 +0300

    link with driver

--- GUI/GUI_Util/mainwindow.cpp
+++ GUI/GUI_Util/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include "device.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    using namespace _onvif;
+    IDevice* device = new Device("192.168.43.196", 8000);
+    device->Init();
 }
 
 MainWindow::~MainWindow()
