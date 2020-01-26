2deaf6426f498cdc0ed73051281be2f4eafdb8fa
Alim <abaev.alim@yandex.ru>
2020-01-26 13:15:41 +0300

    saving auth scheme

--- Driver/Headers/SoapHelpers.h
+++ Driver/Headers/SoapHelpers.h
@@ -15,6 +15,12 @@ namespace _onvif
 	Cause if some data changed it could be easy applied to all services
 	For example, if credential data were changed, there will no be problems
 	***/
+	enum AUTH_SCHEME
+	{
+		NO_AUTH = 0,
+		WSSE_AUTH
+	};
+
 	class ConnectionInfo
 	{
 	public:
@@ -27,10 +33,11 @@ namespace _onvif
 			ip_(address),
 			port_(port),
 			login_(login),
-			password_(password)
-		{
-			
-		}
+			password_(password),
+			auth_scheme_(NO_AUTH)
+		{}
+
+		~ConnectionInfo(){}
 
 		~ConnectionInfo()
 		{
@@ -48,6 +55,8 @@ namespace _onvif
 		int getPort() { return port_; }
 		std::string getLogin() { return login_; }
 		std::string getPass() { return password_; }
+		AUTH_SCHEME auth_scheme() const { return auth_scheme_; }
+		void set_auth_scheme(AUTH_SCHEME new_scheme) { auth_scheme_ = new_scheme; }
 
 	private:
 		soap* soap_context_;
@@ -55,15 +64,18 @@ namespace _onvif
 		short port_;
 		std::string login_;
 		std::string password_;
+		AUTH_SCHEME auth_scheme_;
 	};
 
 	/***
 		This method is intended to implement the logic of invocation
 		GSoap classes' methods with authorization
 		To use it pass to this function some GSoap method
-		for convinience may be at first define lyamba and then pass it to the function
+		for convinience may be at first define lyamba and then pass it to the function.
+		If returned result on client side is 401, it means that credentials are wrong.
+		In this case a client should changed the credentials.
+		
 		Example:
-
 		auto wrapper = [this](_tds__GetCapabilities* r1, _tds__GetCapabilitiesResponse& r2) {return deviceProxy->GetCapabilities(r1, r2); };
 		GSoapRequestWrapper<_tds__GetCapabilities, _tds__GetCapabilitiesResponse>(wrapper, &request, response);
 
@@ -75,13 +87,24 @@ namespace _onvif
 	template <typename R1, typename R2>
 	int GSoapRequestWrapper(std::function<int(R1*, R2&)> f, R1* r1, R2& r2, ConnectionInfo* connInfo)
 	{
-		int res = f(r1, r2);
-
-		if (res == 401)
+		int res;
+		switch (connInfo->auth_scheme())
 		{
+		case NO_AUTH:
+			res = f(r1, r2);
+
+			if (res == 401)
+			{
+				//try all further requests with auth
+				connInfo->set_auth_scheme(WSSE_AUTH);
+			}
+			else
+				break; //requst result not is 401, skip
+		case WSSE_AUTH:
 			soap_wsse_add_UsernameTokenDigest(connInfo->getSoap(), "Auth",
 				connInfo->getLogin().c_str(), connInfo->getPass().c_str());
-			res = f(r1, r2);
+			res = f(r1, r2); //TODO: if res also 401 client should change credentials
+			break;
 		}
 
 		return res;
