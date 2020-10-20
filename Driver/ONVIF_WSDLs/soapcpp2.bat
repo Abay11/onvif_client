bbc70b996757efb97d84156f7ee9358229e2ea68
Alim <abaev.alim@yandex.ru>
2020-10-20 14:23:58 +0300

    update GSoap files to be able to use Event service

---
+++ Driver/ONVIF_WSDLs/soapcpp2.bat
@@ -0,0 +1,2 @@
+C:\GSoap\gsoap-2.8\gsoap\bin\win32\soapcpp2 -2 -j -CL -IC:\GSoap\gsoap-2.8\gsoap\import -d .\sources .\onvif.h
+pause
---
+++ Driver/ONVIF_WSDLs/wsdl2h.bat
@@ -0,0 +1,3 @@
+C:\GSoap\gsoap-2.8\gsoap\bin\win32\wsdl2h.exe -O4 -P -x -t typemap.dat -Nname -o onvif.h .\devicemgmt.wsdl .\media.wsdl .\event.wsdl .\deviceio.wsdl
+
+pause
