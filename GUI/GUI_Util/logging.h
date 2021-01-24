9a1952c90524897b025f96d7dfffeb0768702fe1
Alim <abaev.alim@yandex.ru>
2021-01-24 18:38:33 +0300

    added logging macros

---
+++ GUI/GUI_Util/logging.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <QDebug>
+#include <QTime>
+
+QDebug operator<<(QDebug dbg, const QTime& time)
+{
+		return dbg << QString("%1:%2:%3.%4")
+					 .arg(time.hour())
+					 .arg(time.minute())
+					 .arg(time.second())
+					 .arg(time.msec());
+}
+
+#define LOG_DEBUG(arg) qDebug() << "[DEBUG]" << QTime::currentTime() << __FUNCTION__ << ":" << arg
+#define LOG_INFO(arg) qInfo() << "[INFO]" << QTime::currentTime() << __FUNCTION__ << ": " << arg
+#define LOG_WARN(arg) qWarning() << "[WARN]" << QTime::currentTime() << __FUNCTION__ << ": " << arg
+#define LOG_ERROR(arg) qError() << "[ERROR]" << QTime::currentTime() << __FUNCTION__ << ": " << arg
